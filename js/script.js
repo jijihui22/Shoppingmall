cca9842b3730f5ebec55b8cb415bb3777f1e6ece
jijihui22 <96766525+jijihui22@users.noreply.github.com>
2023-11-09 10:15:03 +0900

    change1

--- js/script.js
+++ js/script.js
@@ -1,31 +1,68 @@
+// popup
+let popup = document.querySelector('.popup'),
+  closeBtn = popup.querySelector('#close'),
+  dayCheck = popup.querySelector('#day-check');
+
+function setCookie(name, value, day) {
+  let date = new Date();
+  date.setDate(date.getDate() + day);
+  document.cookie = `${name}=${value};expires=${date.toUTCString()}`;
+}
+
+function cookieCheck(name) {
+  let cookieArr = document.cookie.split(';');
+  let visited = false;
+
+  for (let cookie of cookieArr) {
+    if (cookie.search(name) > -1) {
+      visited = true;
+      break;
+    }
+  }
+
+  if (!visited) {
+    popup.setAttribute('open', '');
+  }
+};
+cookieCheck('ABC');
+
+closeBtn.addEventListener('click', () => {
+  popup.removeAttribute('open');
+  if (dayCheck.checked) {
+    setCookie('ABC', 'home', 1);
+  } else {
+    setCookie('ABC', 'home', -1);
+  }
+});
+
 // <!-- Initialize Swiper  -->
 var swiper = new Swiper(".mySwiper1", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  spaceBetween:40,
+  spaceBetween: 40,
   slidesPerView: "auto",
   coverflowEffect: {
-  rotate: 50,
-  stretch: 0,
-  depth: 100,
-  modifier: 1,
-  slideShadows: true,
-  loop:true,
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+    loop: true,
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable:true,
+    clickable: true,
   },
   navigation: {
-  nextEl: '.swiper-button-next',
-  prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   autoplay: {
-  delay: 3000,
+    delay: 3000,
   },
 });
-  
+
 //<!- Swiper  2  ->
 var swiper = new Swiper(".mySwiper2", {
   slidesPerView: 4,
@@ -33,34 +70,35 @@ var swiper = new Swiper(".mySwiper2", {
   spaceBetween: 30,
   slidesPerView: "auto",
   grabCursor: true,
-  loop:true,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-  nextEl: '.swiper-button-next',
-  prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   autoplay: {
-  delay: 3000,
+    delay: 3000,
   },
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
   $(".modalWrap a").colorbox({
-      rel:"youtubes"
+    rel: "youtubes"
   });
   $(".youtubes").colorbox({
-      slideshowAuto:true,  
-      iframe:true,
-      innerWidth:1000,
-      innerHeight:562.5,
+    slideshowAuto: true,
+    iframe: true,
+    innerWidth: 1000,
+    innerHeight: 562.5,
   });
 });
-    
+
 var url = "http://www.ftc.go.kr/bizCommPop.do?wrkr_no=1328170229"
-  $(document).ready(function(){
-  $('.ftc_btn').click(function(){window.open(url, "bizCommPop", "width=750, height=700;");return false;
+$(document).ready(function () {
+  $('.ftc_btn').click(function () {
+    window.open(url, "bizCommPop", "width=750, height=700;"); return false;
   });
 });
